64775089552b5f89702f1382ad50f97150e2bd96
Devindi Rajapakse <71745965+devindirajapakse@users.noreply.github.com>
2023-01-24 10:34:34 +0530

    update

---
+++ src/app/addcart/index.js
@@ -0,0 +1,21 @@
+paypal.Buttons({
+   
+    createOrder: function (data, actions) {
+        return actions.order.create({
+            purchase_units : [{
+                amount: {
+                    value: '0.1'
+                }
+            }]
+        });
+    },
+    onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+            console.log(details)
+            window.location.replace("http://localhost:63342/tutorial/paypal/success.php")
+        })
+    },
+    onCancel: function (data) {
+        window.location.replace("http://localhost:63342/tutorial/paypal/Oncancel.php")
+    }
+}).render('#paypal-payment-button');
